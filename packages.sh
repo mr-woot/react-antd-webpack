fbd99520af3fd1cd7beafe86c288c42d227ae8f6
mr-woot <tusharmudgal3@gmail.com>
2019-03-11 22:41:26 +0530

    - Updated dependencies

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 npm i @babel/core @babel/plugin-proposal-class-properties @babel/plugin-proposal-export-namespace-from @babel/plugin-proposal-throw-expressions @babel/plugin-syntax-dynamic-import @babel/polyfill @babel/preset-env @babel/preset-react babel-loader copy-webpack-plugin css-loader html-webpack-plugin mini-css-extract-plugin node-sass optimize-css-assets-webpack-plugin sass-loader style-loader uglifyjs-webpack-plugin webpack webpack-cli webpack-dev-server webpack-merge webpack-visualizer-plugin -D
-npm i react@next react-dom@next -S
+npm i express react react-dom@next antd -S
